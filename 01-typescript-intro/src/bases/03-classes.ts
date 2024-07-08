6a6d5cc6bee38904596f66d2c7b0852424444443
francisram87 <frlopez@bepsa.com.py>
2024-07-08 11:46:34 -0400

    metodos async y resp hhtp

--- 01-typescript-intro/src/bases/03-classes.ts
+++ 01-typescript-intro/src/bases/03-classes.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { Move, PokeApiresponse } from "../interfaces/pokeapi-response.interface";
 export class Pokemon {
 
 //nivel standart
@@ -29,6 +31,18 @@ constructor(
                 console.log(`${this.name}!!!`);
             }
 
+           async getMoves(): Promise<Move[]>{
+               // return 10;
+
+              //  const moves = 0;
+                //const resp = await axios.get('https://pokeapi.co/api/v2/pokemon/4'); // datos estructurados
+                const {data} = await axios.get<PokeApiresponse>('https://pokeapi.co/api/v2/pokemon/4'); // datos des estructurados
+                //console.log(resp.data.moves);
+                 console.log(data.abilities);
+
+                return data.moves;
+            }
+
 }
 
 
@@ -36,3 +50,7 @@ constructor(
 export const charmander1 = new Pokemon(4,"charmander");
 console.log(charmander1);
 charmander1.scream();
+
+//console.log(charmander1.getMoves());
+
+charmander1.getMoves();
